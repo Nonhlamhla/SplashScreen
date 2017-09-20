0eb489ffe21d073a9fc111d5315becc8ba03f94b
Nonhlamhla <mkhonza.s@live.com>
2017-09-20 10:25:15 +0200

    WITH LANDING SCREEN

--- app/src/main/java/com/example/admin/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/admin/splashscreen/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.admin.splashscreen;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    //SHOWS THE NOTIFICATION ICON ON THE XML OF THIS ACTIVITY
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.notification, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
+
